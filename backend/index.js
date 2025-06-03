1e7ea4b761fc9833fa9fb7c6e5fce87670ed5ed1
Shubham Ingole <shubhamingole169@gmail.com>
2025-06-03 21:55:15 +0530

    frontend uri added in .env file

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    // origin:'http://localhost:5173',
+    origin:'process.env.FRONT_END_URI',
     credentials:true
 }
 
