30289191ae222adcff64599a9c04e85f97eb48e6
Shubham Ingole <shubhamingole169@gmail.com>
2025-06-03 22:18:35 +0530

    .env varible added

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -10,16 +10,12 @@
 
 
 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export const USER_API_END_POINT="https://jobs-portal-backend-f9nc.onrender.com/api/v1/user";
-export const JOB_API_END_POINT="https://jobs-portal-backend-f9nc.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT="https://jobs-portal-backend-f9nc.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT="https://jobs-portal-backend-f9nc.onrender.com/api/v1/company";
-
-
-
-
-
+export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
+export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${BASE_URL}/api/v1/application`;
+export const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/company`;
 
 
 
